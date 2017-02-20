dd760e647afec69650643d26caaa950bc8dbf3f9
Maria <mariagiraldo4@gmail.com>
2017-02-20 07:52:01 -0500

    Updating the lates version

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,7 @@ function removeTransition(e){
 }
 
 window.addEventListener('keydown', playSound);
-window.addEventListener('keyup', removeTransition)
+window.addEventListener('keyup', removeTransition);
 
 
 
