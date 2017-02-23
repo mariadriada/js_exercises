ed7e5e5c7a5df71d697f08d6f7093553548ed814
Maria <mariagiraldo4@gmail.com>
2017-02-23 07:43:02 -0500

    Creating speed sound control

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
+
+
 /**
 * Play the sound in english of the key pressed 
 * @param {event} e - Keydown event
 **/
-function playSound(e){     
+function playSound(e){ 
     const audio = document.querySelector(`audio[data-key=${CSS.escape(e.keyCode)}]`);
     const key = document.querySelector(`div[data-key=${CSS.escape(e.keyCode)}]`);
-    
+
     if (!audio) return; //Stop the function
 
     audio.currentTime=0; //rewind 
@@ -28,13 +30,23 @@ function removeTransition(e){
     key.classList.remove('playing');    
 }
 
+/**
+ * change the speed audio to selected
+ */
+function changeSpeedaudio(element) {
+    const  audio = document.querySelectorAll('.speed_control');
+    audio.playBackRate = element.speed
+    console.log('spedd');
+}
+
+
 window.addEventListener('keydown', playSound);
 window.addEventListener('keyup', removeTransition);
 
 
+
 /**
- * To draw sound button in a container (.keys)
- * @constructor
+ * To draw sound button in a container (.keys)  
  */
 (function drawSoundButtons(){
     const EXT_ = ".wav", PREFIX_ = "sounds/";
@@ -51,12 +63,21 @@ window.addEventListener('keyup', removeTransition);
 
     container_keys += "</div>"; //Close .keys
 
-    const prepend =    '\
-                        <div class="bg_text"></div>\
-                        <div class="text"><h1>English Sound Alphabet</h1><p>Please press a letter of the your keyboard for listening its sound in english.</p></div>';
+    const prepend =    '<div class="bg_text"></div> \
+                        <div class="text"> \
+                            <h1>English Sound Alphabet</h1> \
+                            <p>Please press a letter of the your keyboard for listening its sound in english.</p> \
+                        </div> \
+                        ';
+    
+    const append = '<div class="velocity_control"> \
+                        <div class="title_speed_control">Speed control</div>\
+                        <div class="speed_control fast_speed" speed="2">+</div> \
+                        <div class="speed_control normal_speed"  speed="1"></div> \
+                        <div class="speed_control slow_speed" speed="0.5">-</div> \
+                    </div>';
 
-                        console.log(prepend);
-    container_keys = prepend+container_keys;                    
+    container_keys = prepend+container_keys+ append;                   
     //container_keys.insertbefore(prepend, container_keys);
     document.write(container_keys); // Show sound buttons at main page    
 })();
